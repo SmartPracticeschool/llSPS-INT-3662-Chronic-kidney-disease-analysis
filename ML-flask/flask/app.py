21e01045640f56f09c8a364c28086149f2589ea0
sej122 <69914586+sej122@users.noreply.github.com>
2020-09-04 17:28:18 +0530

    Add files via upload

---
+++ ML-flask/flask/app.py
@@ -0,0 +1,41 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+from joblib import load
+app = Flask(__name__)
+model = pickle.load(open('decision.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/y_predict',methods=['POST'])
+def y_predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    x_test = [[float(x) for x in request.form.values()]]
+    print(x_test)
+    sc=load("scalar.save")
+    prediction = model.predict(sc.transform(x_test))
+    print(prediction)
+    output=prediction[0]
+    if(output==0):
+        pred="Patient will have Chronic Kidney Disease"
+    else:
+        pred="Patient will not have Chronic Kidney Disease"
+    return render_template('index.html', prediction_text='Analysis: {}'.format(pred))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.y_predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
